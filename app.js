e47cc2c8a4bfc6a9b3677ff355aaca64c352f269
eunhasublue <eunhasublooming@gmail.com>
2022-01-23 20:02:49 +0900

    ch03_05 router

--- app.js
+++ app.js
@@ -10,26 +10,74 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function () {
-  const id = location.hash.substr(1);
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
+  newsList.push("<ul>");
 
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-  div.innerHTML = `
-    <li>
-      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-    </li>
-  `;
-  ul.appendChild(div.firstElementChild);
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+  <li>
+    <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>
+`);
+  }
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routerPath = location.hash;
+  // location.hash에서 #만 들어올때는 true로 반환한다.
+  if (routerPath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router); //newsDetail);
+
+// 글 목록이 보일려면 router 호출
+router();
+
+// const newsFeed = getData(NEWS_URL);
+// const ul = document.createElement("ul");
+
+// 글 내용 보여주는 함수는 addEventListener 함수에 묶여 있기 때문에 다른데서 호출할 수가 없엇 빼내야 한다.
+// window.addEventListener("hashchange", function () {
+//   const id = location.hash.substr(1);
+//   const newsContent = getData(CONTENT_URL.replace("@id", id));
+//   const title = document.createElement("h1");
+
+//   container.innerHTML = `
+//     <h1>${newsContent.title}</h1>
+//     <div>
+//       <a href="#">목록으로</a>
+//     </div>
+//   `;
+// });
+
+// const newsList = [];
+// newsList.push("<ul>");
+// for (let i = 0; i < 10; i++) {
+//   newsList.push(`
+//   <li>
+//     <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+//   </li>
+// `);
+// }
+// newsList.push("</ul>");
+// container.innerHTML = newsList.join("");
